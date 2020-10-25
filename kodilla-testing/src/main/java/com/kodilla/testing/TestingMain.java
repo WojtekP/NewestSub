3c1da8a453dcf085b16b944ab7b2fface80f38de
Wojciech Pszyk <konkursalfa@wp.pl>
2020-10-25 02:40:42 +0100

    test driven development

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,31 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("FirstUser");
+        Calculator calculator = new Calculator();
+        String result = simpleUser.getUserName();
+
+        if(result.equals("FirstUser")){
+            System.out.println("Test ok");
+
+        } else {
+            System.out.println("Error!");
+        }
+        System.out.println("Adding test...");
+        if(calculator.add(5,7) == (5+7)){
+            System.out.println("adding test is ok");
+        } else {
+            System.out.println("adding error");
+        }
+        System.out.println("Substracting test...");
+        if(calculator.substract(3,2) == (3-2)){
+            System.out.println("Substract test is ok");
+        } else {
+            System.out.println("Substract error");
+        }
     }
 }
