d9432c34ec80e38e73c38cc4a59ed8483e5608bf
Wojciech Pszyk <konkursalfa@wp.pl>
2020-11-06 09:18:23 +0100

    seventh commit

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.*;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
@@ -23,5 +24,8 @@ public class StreamMain {
         poemBeautifier.beautify(("Input Text"),(String) -> String.toLowerCase() );
         poemBeautifier.beautify(("Input Text"),(String) -> String + "!!!" );
         poemBeautifier.beautify(("Input Text"),(String) -> String.replaceAll(".(?!$)", "$0 ") );
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
